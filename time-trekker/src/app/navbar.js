fe48f177d0d504adb1eeaba52b771c87f531c999
Suhaani Garg <suhaanigarg2311@gmail.com>
2025-04-26 17:33:53 +0530

    Editing

--- time-trekker/src/app/navbar.js
+++ time-trekker/src/app/navbar.js
@@ -5,13 +5,13 @@ export default function Navbar() {
     <nav>
       <ul>
         <li><Link href="/">Home</Link></li>
-        <li><Link href="/tasklog">Task Log</Link></li>
         <li><Link href="/dashboard">Dashboard</Link></li>
+        <li><Link href="/tasklog">Task Log</Link></li>
+        <li><Link href="/pomodoro">Pomodoro</Link></li>
         <li><Link href="/about">About</Link></li>
         <li><Link href="/help">Help</Link></li>
         <li><Link href="/login">Login</Link></li>
         <li><Link href="/signup">Signup</Link></li>
-        <li><Link href="/pomodoro">Pomodoro</Link></li>
       </ul>
     </nav>
   );
